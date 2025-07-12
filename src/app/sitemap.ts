ca55c65b3bfafb75868532af90efa30900939c01
One Stop <onestopvk@gmail.com>
2025-07-12 21:37:42 +0000

    Collecting page data  .Error: export const dynamic = "force-static"/expo

--- src/app/sitemap.ts
+++
@@ -1,9 +0,0 @@
-import { MetadataRoute } from 'next';
-
-/**
- * Sitemap generation has been disabled.
- * This file is kept to prevent build errors if a sitemap is expected but can be removed if not needed.
- */
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [];
-}
