e3449edf428d7981ac905a0da7435530659cea65
One Stop <onestopvk@gmail.com>
2025-07-13 19:11:07 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,5 +1,8 @@
 import { BlogPageClient } from '@/components/blog-page-client';
 
+export const dynamic = 'force-static';
+export const dynamicParams = false;
+
 // This function is required for static export of dynamic routes.
 // We return an empty array because we want all rendering to happen on the client.
 export async function generateStaticParams() {
--- src/app/word/[slug]/page.tsx
+++ src/app/word/[slug]/page.tsx
@@ -1,6 +1,9 @@
 
 import { WordPageClient } from '@/components/word-page-client';
 
+export const dynamic = 'force-static';
+export const dynamicParams = false;
+
 // This function is required for static export of dynamic routes.
 // We return an empty array because we want all rendering to happen on the client.
 export async function generateStaticParams() {
